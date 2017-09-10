efc3e7c4ea728e7a9deaffed31433dc273deeb92
Eric  Lau <elau369@icloud.com>
2017-09-10 06:31:01 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -10,3 +10,13 @@ end
 def input_to_index(user_input)
   user_input.to_i - 1
 end
+
+def valid_move?(board, index)
+  if index.between?(1,9)
+    if position_taken?(board, index)
+      true
+    end
+  end
+  index.between?(0,8) && !position_taken?(board, index)
+  
+end
